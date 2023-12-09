c8aa490aaa784e85c33dd9bbf54780bd3b1721ed
Andrew <keenanandrew@gmail.com>
2023-12-09 13:59:38 +0000

    Transformation stage added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data ingestion stage"
 try:
@@ -17,6 +18,16 @@ try:
     logger.info(f">>>>>> {STAGE_NAME} has started <<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f">>>>>> {STAGE_NAME} has finished <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data transformation stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} has started <<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>>>>> {STAGE_NAME} has finished <<<<<<")
 except Exception as e:
     logger.exception(e)
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,6 +1,6 @@
-from src.mlProject.constants import * # this gets the constants constructor file & contents
-from src.mlProject.utils.common import read_yaml, create_directories # importing functions defined in utils
-from src.mlProject.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from mlProject.constants import * # this gets the constants constructor file & contents
+from mlProject.utils.common import read_yaml, create_directories # importing functions defined in utils
+from mlProject.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -43,3 +43,16 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir = config.root_dir, 
+            data_path = config.data_path
+        )
+
+        return data_transformation_config
