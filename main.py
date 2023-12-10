e7745c496788f50168d29ef7c9f2d0da5c5e5f1f
Andrew <keenanandrew@gmail.com>
2023-12-10 18:48:39 +0000

    All stages now complete

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipe
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data ingestion stage"
 try:
@@ -40,6 +41,16 @@ try:
     logger.info(f">>>>>> {STAGE_NAME} has started <<<<<<")
     data_transformation = ModelTrainerTrainingPipeline()
     data_transformation.main()
+    logger.info(f">>>>>> {STAGE_NAME} has finished <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model evaluation stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} has started <<<<<<")
+    model_evaluation = ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
     logger.info(f">>>>>> {STAGE_NAME} has finished <<<<<<")
 except Exception as e:
     logger.exception(e)
